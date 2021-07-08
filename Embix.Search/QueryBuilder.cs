24f574c4dfbf18bee093bb5c1f58cf4fdc54fe35
Myrmex <dfusi@hotmail.com>
2021-07-08 16:29:13 +0200

    Updated packages

--- Embix.Search/QueryBuilder.cs
+++ Embix.Search/QueryBuilder.cs
@@ -5,12 +5,18 @@ using System.Data;
 
 namespace Embix.Search
 {
+    /// <summary>
+    /// Query builder base class.
+    /// </summary>
     public abstract class QueryBuilder
     {
         protected readonly string _connString;
         private QueryFactory _qf;
 
-        protected QueryFactory QueryFactory
+        /// <summary>
+        /// Gets the query factory.
+        /// </summary>
+        public QueryFactory QueryFactory
         {
             get
             {
@@ -22,13 +28,28 @@ namespace Embix.Search
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryBuilder"/> class.
+        /// </summary>
+        /// <param name="connString">The connection string.</param>
+        /// <exception cref="ArgumentNullException">connString</exception>
         protected QueryBuilder(string connString)
         {
             _connString = connString
                 ?? throw new ArgumentNullException(nameof(connString));
         }
 
+        /// <summary>
+        /// Gets the connection.
+        /// </summary>
+        /// <param name="connString">The connection string.</param>
+        /// <returns>The connection.</returns>
         protected abstract IDbConnection GetConnection(string connString);
+
+        /// <summary>
+        /// Gets the SQL compiler.
+        /// </summary>
+        /// <returns>The SQL compiler.</returns>
         protected abstract Compiler GetSqlCompiler();
     }
 }
